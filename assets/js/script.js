7e3eef9eb52f158fc486cf6aa193ad28c9d0ee65
jonathanwhitedev <white_jonathan@icloud.com>
2023-02-02 11:03:47 +0000

    added 15 questions into VAR question bank in JS

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+//Question bank
+var questionBank= [
+    {
+        question : 'What happened in the 1914 Christmas Day truce during the First World War?',
+        option : ['A game of football between British and German soldiers','English comrades did not quarrel','Pope Benedict XV has ascended to the papacy','Everyone had Coca Cola'],
+        answer : 'A game of football between British and German soldiers'
+    },
+    {
+        question : 'Which country annually sends a Christmas Tree to be erected in Londons Trafalgar Square?',
+        option : ['Cananda','Denmark','Norway','Germany'],
+        answer : 'Norway'
+    },
+    {
+        question : 'What north-eastern US state holds the Guiness world record for largest Snowman?',
+        option : ['Massachusetts','Rhode Island','Maine','New Hampshire'],
+        answer : 'Maine'
+    },
+    {
+        question : 'How many of Rudolphs eight companions names start with the letter D?',
+        option : ['3','1','2','4'],
+        answer : '3'
+    },
+    {
+        question : 'What do Americans leave for Santa on Christmas Eve?',
+        option : ['Gift List','Baileys Irish Cream','Sherry & Mince Pie','Milk & Cookies'],
+        answer : 'Milk & Cookies'
+    },
+    {
+        question : 'In Home Alone, where are the McCallisters going on vacation when they leave Kevin behind?',
+        option : ['London','Paris','New York','Milan'],
+        answer : 'Paris'
+    },
+    {
+        question : 'In what country did Silent Night originate?',
+        option : ['Italy','England','Austria','Russia'],
+        answer : 'Austria'
+    },
+    {
+        question : 'The French word "Noel" is often used around Christmas, but what was its original meaning in Latin?',
+        option : ['Night','Merry','Birth','New'],
+        answer : 'Birth'
+    },
+    {
+        question : 'What are you supposed to do when you find yourself under the mistletoe?',
+        option : ['Kiss','Hug','Dance','Bow'],
+        answer : '3'
+    },
+    {
+        question : 'What gifts did The Three Wise Men give Jesus on his birthday?',
+        option : ['Bread, Wine, And Diamond','Gold, Saphire, and Topaz','Gold, Frankincense, and Myrrh','Amber, Emerald, and Ruby'],
+        answer : 'Gold, Frankincense, and Myrrh'
+    },
+    {
+        question : 'Which ocean can Christmas Island be found in?',
+        option : ['Indian','Pacific','Atlantic','Arctic'],
+        answer : 'Indian'
+    },
+    {
+        question : 'What is the best selling Christmas single of all time in the UK?',
+        option : ['Bing Crosby - White Christmas','Band Aid - Do They Know its Christmas?','Mariah Carey - All I want for Christmas is you','Wham! - Last Christmas'],
+        answer : 'Band Aid - Do They Know its Christmas?'
+    },
+    {
+        question : 'Where are the Christmas presents brought by a witch?',
+        option : ['Spain','Italy','Portugal','France'],
+        answer : 'Italy'
+    },
+    {
+        question : 'What is the most popular meal for Christmas in Japan?',
+        option : ['Roast Turkey','KFC Fried Chicken','Turkey Ramen','Sushi'],
+        answer : 'KFC Fried Chicken'
+    },
+    {
+        question : 'In which year was the first Christmas card sent?',
+        option : ['1925','1890','1912','1843'],
+        answer : '1843'
+    }
+]
